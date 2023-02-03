493a38c5f86fe832b5d3321ae6ab8c09aed68b2e
c25tc <c25tc@dalton.org>
2023-02-03 12:41:52 -0500

    adding anylitics

--- firebase.js
+++ firebase.js
@@ -23,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
